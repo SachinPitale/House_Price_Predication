a85cabc187a6da4d3adad6868928f7466224a780
SachinPitale <sachinpitale22@gmail.com>
2022-07-22 10:29:37 +0530

    added all component files

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask
-
+from  housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app=Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def index():
+    try:
+        raise Exception(" testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("testing logger module")    
     return "Machine Learning Project CI CD pipeline"
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
 
 
 
